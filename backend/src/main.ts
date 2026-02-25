cc3a1fc8a7c99ee60bae44da6604026ae4f8edc3
ngocthuan2052 <lengocthuan093@gmail.com>
2026-02-26 02:14:19 +0700

    chore: config database connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://127.0.0.1:3000',
+    Credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT || 3001);
+  console.log('Backend running on http://localhost:3001');
 }
 bootstrap();
