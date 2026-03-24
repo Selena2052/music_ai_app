057c6d3228654fa91ccd6cfaa8c2c62df08a9641
Selena2052 <ngocthuandeptrai12@gmail.com>
2026-03-24 19:35:56 +0700

    fix: add Vercel URL to CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,12 +12,8 @@ async function bootstrap() {
   // cho phép tất cả localhost port
   app.enableCors({
     origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
       'http://localhost:3002',
-      'http://localhost:3003',
-      'http://127.0.0.1:3000',
-      'http://127.0.0.1:3002',
+      'https://music-ai-frontend-nine.vercel.app',
     ],
     credentials: true,
   });
